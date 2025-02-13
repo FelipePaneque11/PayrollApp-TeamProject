a27700b5000110d4a6e7066232363921b2fbd9f7
Murilo Batiuk <152316191+Batiuk89@users.noreply.github.com>
2025-02-13 10:51:33 +0000

    Add files via upload

---
+++ EmployeeSalaryApp/src/employeesalary/EmployeeSalaryApp.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package employeesalary;
+
+/**
+ * EmployeeSalaryApp.java
+ * February 2025
+ * @author Murilo Batiuk
+ */
+public class EmployeeSalaryApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {      
+
+          EmployeeSalaryGUI appGUI = new EmployeeSalaryGUI();
+          appGUI.setVisible(true);
+        
+    }
+    
+}
